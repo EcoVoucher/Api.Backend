66ebbe70de9f0b9add3feb3a3e5ce333bb124b07
joao-pedro01 <joao_pedro01@terra.com.br>
2024-06-10 10:59:34 -0300

    Teste Vercel

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import app from './app.js';
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Servidor escutando em http://localhost:${port}`);
+    console.log(`Server ready on port ${port}`);
 });
