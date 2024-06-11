d4cf10c428921255a84ce8b349d71a72f60ce98d
joao-pedro01 <joao_pedro01@terra.com.br>
2024-06-10 21:14:03 -0300

    Add teste

---
+++ src/controllers/tests/user.test.js
@@ -0,0 +1,80 @@
+const request = require('supertest');
+
+const baseURL = 'http://localhost:3000/api';
+
+describe('API REST de Usuarios sem o Token', () => {
+    it('GET /user - Lista todos os prestadores sem o token', async () => {
+        const response = await request(baseURL)
+            .get('/user')
+            .set('Content-Type', 'application/json')
+            .expect(401); // Forbidden
+    });
+});
+
+describe('API REST de Prestadores com o token', ()=> {
+    let token //Armazenaremos o access_token JWT
+    it('POST - Autenticar usuário para retornar token JWT', async() => {
+        const senha = process.env.SENHA_USUARIO
+        const response = await request(baseURL)
+        .post('/user/login')
+        .set('Content-Type','application/json')
+        .send({"identidade":"36070342810","senha": "teste@11"})
+        .expect(200) //OK
+
+        token = response.body.access_token
+        expect(token).toBeDefined() // Recebemos o token?
+    })
+
+    it('GET - Listar os usuários com autenticação', async() => {
+        const response = await request(baseURL)
+        .get('/user')
+        .set('Content-Type','application/json')
+        .set('access-token', token) //Inclui o token na chamada
+        .expect(200)
+
+        const usuarios = response.body
+        expect(usuarios).toBeInstanceOf(Array)
+    })
+
+    dadosEmpresa = {
+        "nomeEmpresa": "João Pedro",
+        "cnpj": "234567",
+        "email": "joao@joao.com",
+        "senha": "123456",
+        "telefone": 15981212171,
+        "cep": 18050000,
+        "endereco": "Av armando pannunzio",
+        "numero": 1700,
+        "complemento": "apto",
+        "bairro": "jd vera cruz",
+        "cidade": "sorocaba",
+        "estado": "sp"
+    }
+
+    it('POST - Inclui um nova empresa sem autenticação', async() => {
+        const response = await request(baseURL)
+        .post('/user/cadastro')
+        .set('Content-Type','application/json')
+        .send(dadosEmpresa)
+        .expect(201) //Created
+
+        expect(response.body).toHaveProperty('message')
+        expect(response.body.message).toBe("Empresa cadastrada com sucesso")
+
+        expect(response.body).toHaveProperty('error')
+        expect(typeof response.body.error).toBe('boolean')
+    })
+
+    it('DELETE - Deleta um usuário com autenticação', async() => {
+        const response = await request(baseURL)
+        .delete(`/user/${usuarios[0]._id}`)
+        .set('Content-Type','application/json')
+        .set('access-token', token) //Inclui o token na chamada
+        
+    })
+
+
+
+
+
+});
--- src/routes/pegadaRoute.js
+++ src/routes/pegadaRoute.js
@@ -12,32 +12,9 @@ import auth from '../middlewares/auth.js';
 const router = express.Router();
 
 
-/**
- * GET /api/prestadores
- * Lista todos os prestadores de serviço
- * Parâmetros: limit, skip e order
- */
 router.get('/', auth, getPegada);
-
-/**
- * GET /api/prestadores/id/:id
- * Lista o prestador de serviço pelo id
- * Parâmetros: id
- */
 router.get('/id/:id', auth, getPegadaById);
-
-/**
- * GET /api/prestadores/razao/:filtor
- * Lista o prestador de serviço pela razão social
- * Parâmetros: filtro
- */
 router.get('/razao/:filtro', auth, getPegadaByRazao);
-
-/**
- * DELETE /api/prestadores/:id
- * Remove o prestador de serviço pelo id
- * Parâmetros: id
- */
 router.delete('/:id', auth, deletePegada);
 
 export default router
